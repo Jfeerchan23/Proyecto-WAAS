b10ac7346b0810f3b3db8e242f5c1d5b58104130
Alexis Ake <AlexisAkeVela@outlook.com>
2023-05-17 22:18:02 -0600

    Envio de codigo QR en el correo

--- Servidor/src/routes/citaRoutes.js
+++ Servidor/src/routes/citaRoutes.js
@@ -6,6 +6,5 @@ citaRoutes.post('/citas/crear/:idMedico',citaController.crearCitas);
 citaRoutes.put('/citas/reservar/:id', citaController.reservar);
 citaRoutes.get('/citasDisponibles', citaController.citasDisponibles);
 citaRoutes.get('/citasProgramadas', citaController.citasProgramadas);
-citaRoutes.get('/notificar', citaController.notificar);
 
 module.exports = citaRoutes;
