7d0aac22ec7786255cd011659b0786d7a645a7db
Dani2507 <46658875+Dani2507@users.noreply.github.com>
2023-05-20 17:15:05 -0600

    Add files via upload

--- Servidor/src/controllers/loginController.js
+++ Servidor/src/controllers/loginController.js
@@ -1,20 +1,108 @@
+const jwt = require('jsonwebtoken');
 const loginController = {}
+const mysql = require('mysql')
 
 loginController.login = (req, res) => {
-    req.getConnection((err, conn) => {
-        if (err) return res.send(err)
-        //Que los roles sean por numeros
-       /*  paciente-1
-        medico-2
-        recepcion-3
-        admin-4 */
-         const datos ={
-            id:1,
-            rol:2
-         }
-       res.json(datos);
-      })
+
+const correo=req.body.correo;
+const password=req.body.password;
+
+console.log(correo, password);
+
+  //Conexion con la base de datos//
+    const conection = mysql.createConnection({
+      host:'localhost',
+      user:'root',
+      password:'',
+      database:'nimbo'
+    })
+    conection.connect( (err) => {})
+
+    //Se realizan las consultas para obtener datos y saber el tipo de usuario - Tipo Paciente//
+    conection.query('SELECT correoPaciente, contrasenaPaciente, idPaciente FROM `pacientes` WHERE correoPaciente="'+correo+'" AND contrasenaPaciente="'+password+'"',(err, rows) =>{
+      if(rows[0]!=null){
+        const usuario=rows[0];
+        console.log(usuario.idPaciente);
+        
+        req.getConnection((err, conn) => {
+
+          console.log('paso');
+              const payload = {
+                check:true
+            };
+            //Aqui se indica en cuanto tiempo expira el token
+            const token = jwt.sign(payload, "clavesupermegasecreta",{
+              expiresIn: '7d'
+            });
+      
+            if (err) return res.send(err)
+            const datos ={
+              id:usuario.idPaciente,
+              rol:1,
+              token:token
+            }
+            res.json(datos);
+        })
+      }
+    })
     
+    //Se realizan las consultas para obtener datos y saber el tipo de usuario - Tipo Medico//
+    conection.query('SELECT correoMedico, contrasenaMedico, idMedico FROM `medicos` WHERE correoMedico="'+correo+'" AND contrasenaMedico="'+password+'"',(err, rows) =>{
+      if(rows[0]!=null){
+        const usuario=rows[0];
+        console.log(usuario.correoMedico);
+        
+        req.getConnection((err, conn) => {
+
+          console.log('paso');
+              const payload = {
+                check:true
+            };
+            //Aqui se indica en cuanto tiempo expira el token
+            const token = jwt.sign(payload, "clavesupermegasecreta",{
+              expiresIn: '7d'
+            });
+      
+            if (err) return res.send(err)
+            const datos ={
+              id:usuario.idMedico,
+              rol:2,
+              token:token
+            }
+            res.json(datos);
+        })
+      }
+    })
+
+    //Se realizan las consultas para obtener datos y saber el tipo de usuario - Tipo Recepcionista//
+    conection.query('SELECT correoRecepcionista, contrasenaRecepcionista, idRecepcionista FROM `recepcionistas` WHERE correoRecepcionista="'+correo+'" AND contrasenaRecepcionista="'+password+'"',(err, rows) =>{
+      if(rows[0]!=null){
+        const usuario=rows[0];
+        console.log(usuario.correoRecepcionista);
+        
+        req.getConnection((err, conn) => {
+
+          console.log('paso');
+              const payload = {
+                check:true
+            };
+            //Aqui se indica en cuanto tiempo expira el token
+            const token = jwt.sign(payload, "clavesupermegasecreta",{
+              expiresIn: '7d'
+            });
+      
+            if (err) return res.send(err)
+            const datos ={
+              id:usuario.idRecepcionista,
+              rol:3,
+              token:token
+            }
+            res.json(datos);
+        })
+      }
+    })
+    
+   
 };
 
 module.exports = loginController;
